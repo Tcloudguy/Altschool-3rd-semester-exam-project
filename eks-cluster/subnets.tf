df1cc271ad6cae4488a4bca22e61887ccdf4803c
Tcloudguy <walewalls1@gmail.com>
2023-03-20 23:45:36 +0100

    myexamproject

--- eks-cluster/subnets.tf
+++ eks-cluster/subnets.tf
@@ -1,48 +1,48 @@
-resource "aws_subnet" "private-eu-west-3" {
+resource "aws_subnet" "private-eu-west-3a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.0.0/19"
-  availability_zone = "eu-west-3"
+  availability_zone = "eu-west-3a"
 
   tags = {
-    "Name"                            = "private-eu-west-3"
+    "Name"                            = "private-eu-west-3a"
     "kubernetes.io/role/internal-elb" = "1"
     "kubernetes.io/cluster/cicd-cluster"      = "owned"
   }
 }
 
-resource "aws_subnet" "private-eu-west-3" {
+resource "aws_subnet" "private-eu-west-3b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.32.0/19"
-  availability_zone = "eu-west-3"
+  availability_zone = "eu-west-3b"
 
   tags = {
-    "Name"                            = "private-eu-west-3"
+    "Name"                            = "private-eu-west-3b"
     "kubernetes.io/role/internal-elb" = "1"
     "kubernetes.io/cluster/cicd-cluster"      = "owned"
   }
 }
 
-resource "aws_subnet" "public-eu-west-3" {
+resource "aws_subnet" "public-eu-west-3a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.64.0/19"
-  availability_zone       = "eu-west-3"
+  availability_zone       = "eu-west-3a"
   map_public_ip_on_launch = true
 
   tags = {
-    "Name"                       = "public-eu-west-3"
+    "Name"                       = "public-eu-west-3a"
     "kubernetes.io/role/elb"     = "1"
     "kubernetes.io/cluster/cicd-cluster" = "owned"
   }
 }
 
-resource "aws_subnet" "public-eu-west-3" {
+resource "aws_subnet" "public-eu-west-3b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.96.0/19"
-  availability_zone       = "eu-west-3"
+  availability_zone       = "eu-west-3b"
   map_public_ip_on_launch = true
 
   tags = {
-    "Name"                       = "public-eu-west-3"
+    "Name"                       = "public-eu-west-3b"
     "kubernetes.io/role/elb"     = "1"
     "kubernetes.io/cluster/cicd-cluster" = "owned"
   }
