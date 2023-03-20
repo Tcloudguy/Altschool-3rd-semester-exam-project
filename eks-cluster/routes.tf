da49bf12ef07c547e0043758b3b221991cd12253
Tcloudguy <walewalls1@gmail.com>
2023-03-20 23:55:38 +0100

    myexamproject

--- eks-cluster/routes.tf
+++ eks-cluster/routes.tf
@@ -50,22 +50,22 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "private-eu-west-3" {
-  subnet_id      = aws_subnet.private-eu-west-3.id
+resource "aws_route_table_association" "private-eu-west-3a" {
+  subnet_id      = aws_subnet.private-eu-west-3b.id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "private-eu-west-3" {
-  subnet_id      = aws_subnet.private-us-west-3.id
+  subnet_id      = aws_subnet.private-us-west-3a.id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "public-eu-west-3" {
-  subnet_id      = aws_subnet.public-eu-west-3.id
+  subnet_id      = aws_subnet.public-eu-west-3b.id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "public-eu-west-3" {
-  subnet_id      = aws_subnet.public-eu-west-3.id
+  subnet_id      = aws_subnet.public-eu-west-3a.id
   route_table_id = aws_route_table.public.id
   }
